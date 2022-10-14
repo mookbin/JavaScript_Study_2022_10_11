c6d6ad29581cf4e49f19eab60460a52808cfad61
mookbin <beeen1@naver.com>
2022-10-14 13:47:15 +0900

    학습

---
+++ JS_002/js/main03.js
@@ -0,0 +1,26 @@
+const inputNum = document.querySelector("#num");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+  const txtNum = inputNum.value;
+  const intNum = Number(txtNum);
+
+  // input box에 입력된 숫자가 0이면
+  //0보다 큰 수를 입력하세요 라고 alert 보이기
+  //if (Number(txtNum) === 0) {
+  if (intNum <= 0) {
+    alert("0보다 큰 값을 입력하세요");
+  }
+  /**
+   * 숫자일 경우 범위를 검사하는 방법
+   *
+   * intNum 변수 값이 0 이상 :
+   * intNum >= 0, 0 <=intNum
+   * intNum 변수 값이 100 이하 :
+   * intNum <= 100
+   * intNum 변수 값이 0보다 큰 :
+   *   intNum > 0
+   * intNum 변수 값이 100보다 작은 :
+   * intNum < 100
+   */
+});
---
+++ JS_002/js/main07.js
@@ -0,0 +1,28 @@
+/**
+ * input box 에 숫자를 입력하고 확인 버튼 클릭
+ * 1. 입력 값이 없으면 alert ("값을 입력하세요")
+ * 2. 입력 값이 0 이하이면 alert ("0 이상을 입력하세요")
+ * 3. 입력 값이 0보다 크고, 100 이하이면
+ * 값을 제곱하여 alert
+ * 4. 입력 값이 100 보다 크고 200 이하이면
+ * 입력 값 + 100 을 실행하여 alert
+ * 5. 입력 값이 200 보다 크면
+ * 입력 값을 그대로 alert
+ */
+const inputNum = document.querySelector("#num");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+  const txtNum = inputNum.value;
+  if (!txtNum) {
+    alert("값을 입력하세요");
+  } else if (Number(txtNum) <= 0) {
+    alert("0 이상을 입력하세요");
+  } else if (Number(txtNum) <= 100) {
+    alert(Number(txtNum) ** 2);
+  } else if (Number(txtNum) <= 200) {
+    alert(Number(txtNum) + 100);
+  } else {
+    alert(Number(txtNum));
+  }
+});

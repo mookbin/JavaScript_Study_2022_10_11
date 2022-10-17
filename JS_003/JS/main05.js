ca33fa8eae386919c035f3caad55d5c299ecba66
mookbin <beeen1@naver.com>
2022-10-17 15:18:40 +0900

    학습

---
+++ JS_003/JS/main05.js
@@ -0,0 +1,52 @@
+let num1 = 30;
+document.writeln(num1);
+
+// 변수에 값 할당(저장, 대입)
+//변수에 값을 저장하면 이전에 어떤 값이 저장되어있던지 상관없이
+//모두 clear 되고 새롭게 저장한 값만 보관된다
+num1 = 40;
+document.writeln(num1);
+document.writeln("<br/>");
+
+num1 = num1 + 10;
+num1 = num1 + 10;
+num1 = num1 + 10;
+num1 = num1 + 10;
+num1 = num1 + 10;
+document.write(num1);
+
+num1 = num1 + 1;
+num1 = num1 + 1;
+num1 = num1 + 1;
+num1 = num1 + 1;
+num1 = num1 + 1;
+
+// num1 = num1 + 10 을 줄여서 표현하는 식
+num1 += 10;
+
+// num1 = num1 + 1
+num1 += 1;
+
+// num1 = num1 - 1
+num1 -= 1;
+
+//num = num1 * 2
+num1 *= 2;
+
+// num1 = num1 / 2
+
+num1 /= 2;
+
+// num1 변수의 값을  1 증가시켜라
+num1 = num1 + 1;
+num1 += 1;
+num1++;
+++num1;
+// num1 ** : 불가능한 코드,
+//연속연산자(++, --, 증감연산자) 는 덧셈과 뺄셈만 가능
+
+// num1 변수의 값을 1 감소시켜라
+num1 = num1 - 1;
+num1 -= 1;
+num1--;
+--num1;
---
+++ JS_003/JS/main07.js
@@ -0,0 +1,42 @@
+const nums = [3, 5, 1, 2, 3, 4, 5, 6, 7, 8];
+// nums 배열에 담긴 각각 요소의 값을 console 에 출력
+
+console.log("첫 번째", nums[0]);
+console.log("두 번째", nums[1]);
+console.log("세 번째", nums[2]);
+console.log("네 번째", nums[3]);
+console.log("다섯 번째", nums[4]);
+console.log("여섯 번째", nums[5]);
+console.log("일곱 번째", nums[6]);
+console.log("여덟 번째", nums[7]);
+console.log("아홉 번째", nums[8]);
+console.log("열 번째", nums[9]);
+
+let index = 0;
+console.log("첫 번째", nums[index++]);
+console.log("두 번째", nums[index++]);
+console.log("세 번째", nums[index++]);
+console.log("네 번째", nums[index++]);
+console.log("다섯 번째", nums[index++]);
+console.log("여섯 번째", nums[index++]);
+console.log("일곱 번째", nums[index++]);
+console.log("여덟 번째", nums[index++]);
+console.log("아홉 번째", nums[index++]);
+console.log("열 번째", nums[index++]);
+
+for (let i = 0; i < 10; i++) {
+  console.log(i + 1, "번 째", nums[i]);
+}
+
+// 배열의 개수에 관계없이 전체 요소를 출력하기
+for (let i = 0; i < nums.length; i++) {
+  console.log(i + 1, " 번 째", nums[i]);
+}
+
+// nums 배열에 담긴 숫자들의 합을 구하여 출력
+
+let sum = 0;
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i];
+}
+console.log(sum);

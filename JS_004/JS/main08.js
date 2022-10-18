54d5ea70315acb37fdf42b2e1f4a20f5a8948851
“mookbin” <beeen1@naver.com>
2022-10-19 01:43:34 +0900

    과제

--- JS_004/JS/main08.js
+++ JS_004/JS/main08.js
@@ -13,7 +13,29 @@ const nickname = document.querySelector("#nickname");
 const tell = document.querySelector("#tell");
 
 btn_submit?.addEventListener("click", () => {
-    const target = tag.target;
-    if (target.tagName === "") {
-        
-    }
+
+        if (username.value == "") {
+           alert ("아이디를 입력하세요");
+           username.focus();
+        }
+       
+        else if (password.value == ""){
+           alert("비밀번호를 입력하세요")
+           password.focus()
+   
+        }
+        else if (password.value != re_password.value) {
+           alert('패스워드가 일치하지 않습니다.');
+           password.focus();
+        }
+        else if (nickname.value == "") {
+           alert("닉네임을 입력하세요")
+           nickname.focus()
+           }
+           else if (tell.value == "") {
+               alert("전화번호를 입력하세요")
+               tell.focus()
+           } 
+   
+   
+        })

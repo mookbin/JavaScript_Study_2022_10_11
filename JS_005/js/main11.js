24830db3fdf623700cfc6ea09a8965933f473cb5
mookbin <beeen1@naver.com>
2022-10-19 16:56:34 +0900

    수업

--- JS_005/js/main11.js
+++ JS_005/js/main11.js
@@ -3,24 +3,25 @@
  * 입력된  이름, 주소, 전화번호를 table 에 추가
  * 2. 화면의 UI를 각자 디자인
  */
+const inputs = document.querySelectorAll("input");
 const tbodyAddr = document.querySelector("tbody.addr");
 const btn = document.querySelector("button");
-const 이름 = [];
 
 btnOk?.addEventListener("click", () => {
+  const 이름 = [];
   for (let i = 0; i < 이름.length; i++) {
-    const tr = document.createElement("TR");
-
+    let tr = document.createElement("TR");
     let td = document.createElement("TD");
+
     td.innerText = 이름[i];
     tr.appendChild(td);
 
     td = document.createElement("TD");
-    td.innerText = 주소[i];
+    td.innerText = "주소" + i;
     tr.appendChild(td);
 
     td = document.createElement("TD");
-    td.innerText = 전화번호[i];
+    td.innerText = "전화번호" + i;
     tr.appendChild(td);
 
     tbodyAddr.appendChild(tr);

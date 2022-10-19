e01b99d9ac6d611f26f6fd762a6474391428b283
“mookbin” <beeen1@naver.com>
2022-10-20 01:28:22 +0900

    과제

--- JS_005/js/main11.js
+++ JS_005/js/main11.js
@@ -7,23 +7,30 @@ const inputs = document.querySelectorAll("input");
 const tbodyAddr = document.querySelector("tbody.addr");
 const btn = document.querySelector("button");
 
-btnOk?.addEventListener("click", () => {
-  const 이름 = [];
+btn?.addEventListener("click", () => {
+
+  const 이름 = [""] ;
+
   for (let i = 0; i < 이름.length; i++) {
+
+    
     let tr = document.createElement("TR");
     let td = document.createElement("TD");
 
     td.innerText = 이름[i];
     tr.appendChild(td);
+  
 
     td = document.createElement("TD");
     td.innerText = "주소" + i;
     tr.appendChild(td);
+   
 
     td = document.createElement("TD");
     td.innerText = "전화번호" + i;
     tr.appendChild(td);
 
     tbodyAddr.appendChild(tr);
-  }
+
+}
 });
